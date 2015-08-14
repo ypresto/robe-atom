616ad0ee5adc0196404649957d424f661d17bff2
Yuya Tanaka <yuya.presto@gmail.com>
2015-08-15 03:00:02 +0900

    Fix launcher does not exit on signal

--- bin/robe_launcher.rb
+++ bin/robe_launcher.rb
@@ -3,15 +3,16 @@
 robe_ruby_path = ARGV[0]
 port = ARGV[1].to_i
 
-%w(INT TERM).each do |signal|
-  trap(signal) { exit }
-end
-
 unless defined? Robe
   $LOAD_PATH.unshift(robe_ruby_path + '/lib')
   require 'robe'
 end
 p Robe.start(port)
+
+# override Robe's signal trapping
+%w(INT TERM).each do |signal|
+  trap(signal) { exit }
+end
 
 # wait until EOF (= parent exit)
 STDIN.each_line {}
